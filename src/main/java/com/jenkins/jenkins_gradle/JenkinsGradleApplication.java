ffb6ae69018e46c82c5bd0251f492525ffe33a83
miftodii <miftodii@bmj.com>
2019-03-15 16:33:55 +0000

    Initial commit

---
+++ src/main/java/com/jenkins/jenkins_gradle/JenkinsGradleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkins_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGradleApplication.class, args);
+	}
+
+}
